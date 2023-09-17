0ab83013ae4740c28d090f9a4512331915c17f10
Rudolf Stasilovich <75040556+rudiandradi@users.noreply.github.com>
2023-09-18 00:19:01 +0300

    private info

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 
-bot_token = '5101491981:AAGK7RrOLh-8cCb0nQPsr0yU2n7eLnmqjvQ'
-#bot_token ='1978894491:AAH7SGa1L7S8ys_eZOAaROsDNt-DIuw7t-Y'
-#host_db = 'Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;'
+bot_token = 'token'
 
-dbname = 'oxxxy'
-user = 'postgres'
-password = 'root'
-host = '127.0.0.1'
+dbname = 'dbname'
+user = 'user'
+password = 'password'
+host = 'password'
